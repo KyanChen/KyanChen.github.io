443e1675d1ea657c2402f02efd521ffd2813c15c
KyanChen <keyan1055002755@163.com>
2023-03-01 19:35:18 +0800

    add

--- github_myprofile_updater/update.py
+++ github_myprofile_updater/update.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     base_dir = '../_pages/includes/'
     _intro = open(f'{base_dir}/intro.md').read().strip()
     _homepage = open(f'{base_dir}/homepage.md').read().strip()
-    _pub = open(f'{base_dir}/pub_short.md').read().strip()
+    # _pub = open(f'{base_dir}/pub_short.md').read().strip()
     _news = open(f'{base_dir}/news.md').read().strip()
     with open('README.md', 'w') as f:
         f.write(_header)
@@ -14,4 +14,4 @@ if __name__ == '__main__':
         f.write('\n\n##')
         f.write(_news)
         f.write('\n\n##')
-        f.write(_pub)
+        # f.write(_pub)
